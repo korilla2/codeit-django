1f28f0f60c6d5898d979dd90026936b703d92c24
korilla2 <kingdoss@naver.com>
2021-12-22 00:42:55 +0900

    커밋

---
+++ costaurant/foods/views.py
@@ -0,0 +1,17 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# Create your views here.
+
+
+def index(request):
+    today = datetime.today().date()
+    context = {'date': today}
+    return render(request, 'foods/index.html', context=context)
+
+
+def food_detail(request, food):
+    context = {"name": food}
+    return render(request, 'foods/detail.html', context=context)
