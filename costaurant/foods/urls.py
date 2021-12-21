c1382146a92a60f75ced7f763c9943f85c456392
korilla2 <kingdoss@naver.com>
2021-12-22 00:42:49 +0900

    커밋

--- costaurant/foods/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index),
-    path('menu/<str:food>/', views.food_detail)
-]
