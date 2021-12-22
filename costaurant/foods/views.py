395ccffd24a9c30c2d20862b162ed7c189ad93fe
korilla2 <kingdoss@naver.com>
2021-12-22 17:34:20 +0900

    커밋

--- costaurant/foods/views.py
+++ costaurant/foods/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.http import Http404
 
 # Create your views here.
 
@@ -13,5 +14,12 @@ def index(request):
 
 
 def food_detail(request, food):
-    context = {"name": food}
+    context = dict()
+    if food == 'chicken':
+        context['name'] = '코딩에 빠진 닭'
+        context['description'] = '주머니가 가벼운 당신의 마음까지 생각한 가격 !'
+        context['price'] = 10000
+        context['img_path'] = 'foods/images/chicken.jpg'
+    else:
+        raise Http404('이런 음식은 없다구요 !')
     return render(request, 'foods/detail.html', context=context)
