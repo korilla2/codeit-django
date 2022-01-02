bf5d424ffee4208cbbfd2be7b51c42340e11e018
korilla2 <kingdoss@naver.com>
2022-01-02 15:59:50 +0900

    커밋

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
 T = int(input())
 
+month_12 = ['0'+str(x) for x in range(1, 10)]
+month_12 += ['10', '11', '12']
 
-for i in range(T):
+for t in range(T):
+    calendar = input()
+    year, month, days = calendar[0:4], calendar[4:6], calendar[6:8]
 
-    a = list(map(int, input().split()))
-    result = [num for num in a if num % 2 == 1]
-    print(f'#{i+1} {sum(result)}')
+    if month in month_12:
+        if month == '02':
+            if int(days) > 0 and int(days) <= 28:
+                print('#{} {}/{}/{}'.format(t+1, year, month, days))
+            else:
+                print('#{} {}'.format(t+1, -1))
+        elif month in ['04', '06', '09', '11']:
+            if int(days) > 0 and int(days) <= 30:
+                print('#{} {}/{}/{}'.format(t+1, year, month, days))
+            else:
+                print('#{} {}'.format(t+1, -1))
+        else:
+            if int(days) > 0 and int(days) <= 31:
+                print('#{} {}/{}/{}'.format(t+1, year, month, days))
+            else:
+                print('#{} {}'.format(t+1, -1))
+    else:
+        print('#{} {}'.format(t+1, -1))
